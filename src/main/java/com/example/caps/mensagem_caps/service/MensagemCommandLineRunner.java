7b2df10ad7a18cd894b99d03fec361a4d3d8806c
Gustavo Enrick <gustavoenrick16@gmail.com>
2024-10-12 18:49:48 -0300

    created to test

--- src/main/java/com/example/caps/mensagem_caps/service/MensagemCommandLineRunner.java
+++ src/main/java/com/example/caps/mensagem_caps/service/MensagemCommandLineRunner.java
@@ -1,2 +1,27 @@
-package com.example.caps.mensagem_caps.service;public class MensagemCommandLineRunner {
+package com.example.caps.mensagem_caps.service;
+
+import com.example.caps.mensagem_caps.DTO.MensagemDTO;
+import com.example.caps.mensagem_caps.model.MensagemModel;
+import com.example.caps.mensagem_caps.repository.MensagemRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MensagemCommandLineRunner implements CommandLineRunner {
+
+    private final MensagemRepository mensagemRepository;
+
+    public MensagemCommandLineRunner(MensagemRepository mensagemRepository) {
+        this.mensagemRepository = mensagemRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        mensagemRepository.save(new MensagemModel(new MensagemDTO("teste1", "teste teste1")));
+        mensagemRepository.save(new MensagemModel(new MensagemDTO("teste2", "teste teste2")));
+        mensagemRepository.save(new MensagemModel(new MensagemDTO("teste3", "teste teste3")));
+        mensagemRepository.save(new MensagemModel(new MensagemDTO("teste4", "teste teste4")));
+
+        System.out.println("Mensagens inseridas com sucesso!");
+    }
 }
