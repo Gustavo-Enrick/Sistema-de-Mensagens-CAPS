72a6f73b1d15c2b28accd38de1e786f087ebdcfd
Gustavo Enrick <gustavoenrick16@gmail.com>
2024-10-12 18:48:12 -0300

    create first part MVC

--- src/main/java/com/example/caps/mensagem_caps/controller/MVC/MensagemControlerHtml.java
+++ src/main/java/com/example/caps/mensagem_caps/controller/MVC/MensagemControlerHtml.java
@@ -1,13 +1,27 @@
-package com.example.caps.mensagem_caps.controller.html;
+package com.example.caps.mensagem_caps.controller.MVC;
 
+import com.example.caps.mensagem_caps.model.MensagemModel;
+import com.example.caps.mensagem_caps.repository.MensagemRepository;
+import jdk.jfr.Enabled;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
 
 @Controller
-
+@EnableWebMvc
+@RequestMapping("/show")
 public class MensagemControlerHtml {
-    @GetMapping("/mensagens/show")
-    public String mostrarMensagem() {
+    @Autowired
+    private MensagemRepository mensagemRepository;
+    @GetMapping()
+    public String mostrarListaMensagem(Model model) {
+        List<MensagemModel> modelList =  mensagemRepository.findAll();
+        model.addAttribute("mensagens",modelList);
         return "pacoteTv/tv";
     }
 }
